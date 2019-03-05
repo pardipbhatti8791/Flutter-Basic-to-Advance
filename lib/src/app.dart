d4a4b4be4b18713a29118583a2f7c2b4c59f223a
Pardip Bhatti <pardipbhatti@Pardips-iMac.local>
2019-03-06 03:47:33 +0530

    basics setup * Material uses * Scaffold usage * statless and statefull widget * flotingActionButton using scaffold * imageModel for api request and data management;

---
+++ lib/src/app.dart
@@ -0,0 +1,38 @@
+// Import flutter helper library
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              counter += 1;
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+        body: Center(
+          child: Text('$counter'),
+        ),
+      ),
+    );
+  }
+}
+
+// Must define a 'build' method that returns
+//
